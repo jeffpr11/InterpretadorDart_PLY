0a57da55ec03604bb4e125f63b1d32b162ac5c97
xecarlier <59071752+xecarlier@users.noreply.github.com>
2021-11-14 23:33:01 -0500

    Algoritmo prueba

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from analisis_lexico import receiveLex
 
 #Entrada por consola - Xavier Carlier
 usrInput = ""
+algoritmoPrueba = '''
+                    var xval = true;
+                    if (xval == true) {
+                        print(0);
+                    else {
+                        print(1);
+                    }
+                '''
+receiveLex(algoritmoPrueba)
 while (usrInput != 'exit'):
     usrInput = input('Escribe token > ')
     receiveLex(usrInput)
